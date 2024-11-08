fd32f73184ed9e1b1facf7b43482a8d0056e6ef3
DavidRJZhang <zhangruijing65@gmail.com>
2024-11-08 16:51:41 +0800

    Bottom_check

---
+++ Board/Bottom_check.c
@@ -0,0 +1,32 @@
+#include "stm32f10x.h"
+#include "Global.h"
+#include "Bottom_check.h"
+void Bottom_check(void)
+{
+    
+    int i = 0, j = 0;
+    for(i = 0; i < 4; i++){
+        for(j = 0; j < 4; j++){
+            if(block[i][j] != 0){
+                if(Playfield[block_pos_x-1+i][block_pos_y-1+j] != 0){
+                    insert_block;
+                    break;
+                }
+            }
+            
+        }
+    }
+}
+
+void insert_block(void)
+{
+    int i = 0. j = 0;  
+    for(i = 0; i < 4; i++)
+    {
+        for(j = 0; j < 4; j++)
+        {
+            Playfield[block_pos_x+i][block_pos_y+j] = block[i][j];
+        }
+    }
+
+}
---
+++ Board/Bottom_check.h
@@ -0,0 +1,8 @@
+#ifndef Bottom_check
+#define Bottom_check
+#include "Bottom_check.h"
+#include "Global.h"
+void Bottom_check(void);
+void insert_block(void);
+
+#endif
